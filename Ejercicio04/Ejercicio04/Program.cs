e26080e0e3f8d262dfb73d1d3170a39b1f597c90
diego.taub <diego.taub@accenture.com>
2021-03-20 17:55:01 -0300

    Ejercicio04

---
+++ Ejercicio04/Ejercicio04/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (Console.ReadLine() != "fin")
+            {
+
+            }
+        }
+    }
+}
